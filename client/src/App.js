82f88ccced3a9999299a22016722fed3c287974c
NicoxRC <nicorojas005@gmail.com>
2022-10-19 10:10:47 -0500

    creating store and files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const dogsState = useSelector(state => state.dogs);
+  console.log(dogsState);
+
+
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
---
+++ client/src/features/dogs/dogsSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const dogSlice = createSlice({
+    name: 'dog',
+    initialState: [],
+    reducers: {
+
+    }
+});
+
+export default dogSlice.reducer;
