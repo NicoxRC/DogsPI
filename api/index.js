fc92cdad96fc28eb4ab00abc78a305c6d56783e8
NicoxRC <nicorojas005@gmail.com>
2022-11-23 10:43:16 -0500

    listentin at...

--- api/index.js
+++ api/index.js
@@ -23,6 +23,6 @@ const { conn } = require('./src/database/db');
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
